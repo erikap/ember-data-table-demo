800f9b4bc6c19d89d3dbaa71d3919eac5b25f3c2
Erika Pauwels <erika.pauwels@gmail.com>
2017-02-22 23:45:34 +0100

    Add a simple data table

---
+++ app/controllers/books.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import DefaultQueryParamsMixin from 'ember-data-table/mixins/default-query-params';
+
+export default Ember.Controller.extend(DefaultQueryParamsMixin, {
+  enableFilter: true
+});
