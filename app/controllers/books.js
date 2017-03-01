331eef72c1695ce9d1de32172971f9e99660076d
Erika Pauwels <erika.pauwels@gmail.com>
2017-03-01 22:22:38 +0100

    Add size param ^C the data table

--- app/controllers/books.js
+++ app/controllers/books.js
@@ -2,5 +2,6 @@ import Ember from 'ember';
 import DefaultQueryParamsMixin from 'ember-data-table/mixins/default-query-params';
 
 export default Ember.Controller.extend(DefaultQueryParamsMixin, {
-  enableFilter: true
+  enableFilter: true,
+  size: 5
 });
